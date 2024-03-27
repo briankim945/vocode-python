02d638f3bfb075f1e3848b34e037bc86300d4acb
briankim945 <bsk945@gmail.com>
2024-03-27 13:53:43 -0400

    Remove message experimentation with chat_gpt_agent

--- vocode/streaming/agent/chat_gpt_agent.py
+++ vocode/streaming/agent/chat_gpt_agent.py
@@ -184,7 +184,7 @@ class ChatGPTAgent(RespondAgent[ChatGPTAgentConfig]):
             openai_get_tokens(stream), get_functions=True
         ):
             logger_external.debug(f"message in async for: {message}")
-            new_message = BaseMessage(text=str("This is an entirely new message that I've been given"))
-            logger_external.info(f"Now trying to overwrite with message: {new_message}")
-            # yield message, True
-            yield new_message.text, True
+            # new_message = BaseMessage(text=str("This is an entirely new message that I've been given"))
+            # logger_external.info(f"Now trying to overwrite with message: {new_message}")
+            yield message, True
+            # yield new_message.text, True
