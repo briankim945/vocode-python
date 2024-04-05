255cb38ee0304cf2889368ae114656ae18680555
briankim945 <bsk945@gmail.com>
2024-04-04 17:28:14 -0700

    Debugging logger part 4

--- vocode/streaming/output_device/twilio_output_device.py
+++ vocode/streaming/output_device/twilio_output_device.py
@@ -41,8 +41,8 @@ class TwilioOutputDevice(BaseOutputDevice):
     async def process(self):
         while self.active:
             message = await self.queue.get()
-            logger.debug(f"V2: From within twilio_output_device process, message: {message}")
-            logger.debug(f"{'event' in message} and {message['event'] == 'dtmf'}")
+            # logger.debug(f"V2: From within twilio_output_device process, message: {message}")
+            # logger.debug(f"{'event' in message} and {message['event'] == 'dtmf'}")
             logger.debug("Something's rotten in the state of denmark")
             try:
                 if "event" in message and message["event"] == "dtmf":
